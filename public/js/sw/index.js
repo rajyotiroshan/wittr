ca56b8201ac625346b35b1cdd1b344362bd46542
rajan <rajangreedge@gmail.com>
2018-11-17 07:14:22 +0530

    req hijack#2 with fetch#1

--- public/js/sw/index.js
+++ public/js/sw/index.js
@@ -2,11 +2,13 @@ self.addEventListener('fetch', function(event) {
 	// TODO: respond to all requests with an html response
 	// containing an element with class="a-winner-is-me".
 	// Ensure the Content-Type of the response is "text/html"
-	event.respondWith(new Response('Hello<b class="a-winner-is-me">Rajan!</b>',{
+	/*event.respondWith(new Response('Hello<b class="a-winner-is-me">Rajan!</b>',{
 		headers:{
 			'Content-Type':'text/html'
 		}
 	})
-	);
+	);*/
  // console.log(event.request);
+ event.respondWith(
+ 	fetch("/imgs/dr-evil.gif"));
 });
