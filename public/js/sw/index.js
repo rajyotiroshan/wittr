1008f0c5c3d4f0b8355e1c18e4e48f70c41b20a7
rajan <rajangreedge@gmail.com>
2018-11-17 07:05:59 +0530

    hijack req#1

--- public/js/sw/index.js
+++ public/js/sw/index.js
@@ -2,5 +2,11 @@ self.addEventListener('fetch', function(event) {
 	// TODO: respond to all requests with an html response
 	// containing an element with class="a-winner-is-me".
 	// Ensure the Content-Type of the response is "text/html"
-  console.log(event.request);
+	event.respondWith(new Response('Hello<b class="a-winner-is-me">Rajan!</b>',{
+		headers:{
+			'Content-Type':'text/html'
+		}
+	})
+	);
+ // console.log(event.request);
 });
